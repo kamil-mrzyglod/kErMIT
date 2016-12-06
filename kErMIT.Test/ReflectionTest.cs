d79db9416c9aceb53d2910bd93f09e379c54aad0
Kamil Mrzygłód <kamz@oticon.com>
2016-12-06 12:25:09 +0100

    First helper method introduced - creating an instance

---
+++ kErMIT.Test/ReflectionTest.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace kErMIT.Test
+{
+    internal class ReflectionTest
+    {
+        [Test]
+        public void Foo()
+        {
+            var foo = typeof(Foo).CreateInstance();
+
+            Assert.That(foo, Is.InstanceOf<Foo>());
+        }
+    }
+
+    internal class Foo
+    {       
+    }
+}
---
+++ kErMIT/Reflection.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Sigil;
+
+namespace kErMIT
+{
+    public static class Reflection
+    {
+        /// <summary>
+        /// Creates an instance of a type using a contructor
+        /// without parameters
+        /// </summary>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        public static object CreateInstance(this Type type)
+        {
+            return CreateInstance(type, null);
+        }
+
+        public static object CreateInstance(this Type type, params object[] parameters)
+        {
+            var emiter = Emit<Func<object>>.NewDynamicMethod("__kErMIT_Create_Instance");
+
+            using (var local = emiter.DeclareLocal<object>("__instance"))
+            {
+                emiter.NewObject(type);
+                emiter.StoreLocal(local);
+                emiter.LoadLocal(local);
+                emiter.Return();
+
+                var del = emiter.CreateDelegate();
+                return del();
+            }
+        }
+    }
+}
